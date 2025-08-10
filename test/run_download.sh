bc0f12649159efe4650620713ad2fd01150d7d06
jiangyuan liu [jiangyuan.liu5@umassmed.edu] <jiangyuan.liu5-umw@hpcc03.umassrc.org>
2025-08-10 18:50:23 -0400

    Load conda under different shell configurations

--- test/run_download.sh
+++ test/run_download.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -e  # Exit on error
 
-# Load conda (ensure conda command is available)
-source ~/.bashrc
+# Load conda into the shell session
+source "$(conda info --base)/etc/profile.d/conda.sh"
 
 # Activate the 'unionloops-nf' conda environment
 conda activate unionloops-nf
 
 # Run the Python script
-./download_data.py
+python ./download_data.py
