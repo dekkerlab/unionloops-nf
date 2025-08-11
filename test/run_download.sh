5c54364fd60296b6812548bbc779e1b8436bd1ab
jiangyuan liu [jiangyuan.liu5@umassmed.edu] <jiangyuan.liu5-umw@hpcc03.umassrc.org>
2025-08-11 09:55:04 -0400

    Add a echo for users to wait for a while to download

--- test/run_download.sh
+++ test/run_download.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e  # Exit on error
 
+# Inform user about runtime
+echo "Please be patient, this process may take a while..."
+
 # Load conda into the shell session
 source "$(conda info --base)/etc/profile.d/conda.sh"
 
